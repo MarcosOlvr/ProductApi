4db632c4803ac6d7c66d93267d49dfd26212392e
Marcos Oliveira <marquinhos.dev@gmail.com>
2025-02-19 19:58:39 -0300

    add categoria crud

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -60,4 +60,48 @@ public class HomeController : ControllerBase
         _produtoRepository.RemoverProduto(id);
         return NoContent();
     }
+
+    [HttpGet]
+    [Route("categorias")]
+    public async Task<ActionResult<dynamic>> TodasCategorias()
+    {
+        var categorias = _categoriaRepository.TodasCategorias();
+        return Ok(categorias);
+    }
+
+    [HttpGet]
+    [Route("categorias/{id:int}")]
+    public async Task<ActionResult<dynamic>> ObterCategoriasPorId(int id)
+    {
+        var categoria = _categoriaRepository.BuscarCategoriaPorId(id);
+        
+        return Ok(categoria);
+    }
+
+    [HttpPost]
+    [Route("categorias")]
+    public async Task<ActionResult<dynamic>> AdicionarCategoria(Categoria categoria)
+    {
+        _categoriaRepository.AdicionarCategoria(categoria);
+        
+        return StatusCode(201, categoria);  
+    }
+
+    [HttpPut]
+    [Route("categorias")]
+    public async Task<ActionResult<dynamic>> AtualizarCategoria(Categoria categoria)
+    {
+        _categoriaRepository.AtualizarCategoria(categoria);
+        
+        return Ok(categoria);
+    }
+
+    [HttpDelete]
+    [Route("categorias/{id:int}")]
+    public async Task<ActionResult<dynamic>> RemoverCategoria(int id)
+    {
+        _categoriaRepository.RemoverCategoria(id);
+        
+        return NoContent();
+    }
 }
